745c92b236826c06ee296d6b326f44c6dc8428f1
aedobbyn <adobbs11@gmail.com>
2020-02-07 16:50:47 -0500

    add votes_get_by_official

--- R/utils.R
+++ R/utils.R
@@ -46,3 +46,35 @@ as_char_vec <- function(x) {
 elmers <- glue::glue %>>>% as.character
 
 elmers_message <- elmers %>>>% message
+
+# Take the v heterogeneous categories list col and tidy it into the same nested tibble format
+explode_categories <- function(tbl) {
+  for (i in 1:nrow(tbl)) {
+    if (length(tbl$categories[i][[1]]) == 0) {
+      tbl$categories[i] %<>%
+        tibble(
+          category_id = NA,
+          category_name = NA
+        ) %>%
+        list()
+    } else {
+      if (purrr::vec_depth(tbl$categories[i]) > 3) {
+        tbl$categories[i] %<>%
+          purrr::flatten()
+      }
+
+      tbl$categories[i] %<>%
+        purrr::map_df(bind_rows) %>%
+        distinct() %>%
+        rename_all(
+          snakecase::to_snake_case
+        ) %>%
+        rename(
+          category_name = name
+        ) %>%
+        list()
+    }
+  }
+  tbl
+}
+
---
+++ R/votes_get_by_official.R
@@ -0,0 +1,133 @@
+
+#' Get votes by official
+#'
+#' @param candidate_ids Vector of candidate_ids.
+#' @param office_ids Vector of office_ids.
+#' @param category_ids Vector of category_ids.
+#' @param years Vector of years in which the vote was taken.
+#' @param all Boolean: should all possible combinations of the variables be searched for, or just the exact combination of them in the order they are supplied?
+#' @param verbose Should cases when no data is available be messaged?
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' aoc <- candidates_get_by_lastname(
+#'   "ocasio-cortez",
+#'   election_years = "2018"
+#' )
+#' votes_get_by_official(aoc)
+
+votes_get_by_official <- function(candidate_ids,
+                                           office_ids = "",
+                                           category_ids = "",
+                                           years = "",
+                                           all = TRUE,
+                                           verbose = TRUE) {
+  candidate_ids %<>%
+    as_char_vec()
+  office_ids %<>%
+    as_char_vec()
+  category_ids %<>%
+    as_char_vec()
+  years %<>%
+    as_char_vec()
+
+  r <- "Votes.getByOfficial?"
+
+  if (all) {
+    query_df <-
+      expand.grid(
+        candidate_id = candidate_ids,
+        office_id = office_ids,
+        category_id = category_ids,
+        year = years
+      ) %>%
+      mutate(
+        query =
+          elmers(
+            "&candidateId={candidate_id}&officeId={office_id}&categoryId={category_id}&year={year}"
+          )
+      )
+  } else {
+    lengths <-
+      c(length(candidate_ids), length(office_ids), length(category_ids), length(years)) %>%
+      magrittr::extract(
+        !. == 1
+      )
+
+    if (!identical(lengths)) {
+      stop("If `all` is FALSE, lengths of inputs must be equivalent to each other, or 1.")
+    }
+
+    query_df <-
+      tibble(
+        candidate_id = candidate_ids,
+        office_id = office_ids,
+        category_id = category_ids,
+        year = years
+      )
+  }
+
+  out <- tibble()
+
+  for (i in 1:nrow(query_df)) {
+    q <- query_df$query[i]
+    candidate_id <- query_df$candidate_id[i]
+    office_id <- query_df$office_id[i]
+    category_id <- query_df$category_id[i]
+    year <- query_df$year[i]
+
+    if (verbose) {
+      elmers_message(
+        "Requesting data for {{candidate_id: {candidate_id}, office_id: {office_id}, category_id: {category_id}, year: {year}}}."
+      )
+    }
+
+    this <- get(
+      req = r,
+      query = q,
+      level_one = "bills",
+      level_two = "bill"
+    )
+
+    if (all(is.na(this))) {
+      if (verbose) {
+        elmers_message(
+          "No results found for query {q}."
+        )
+      }
+
+      # Other cols will be NA
+      this <-
+        tibble(
+          candidate_id = candidate_id,
+          office_id = office_id,
+          category_id = category_id,
+          year = year
+        ) %>%
+        purrr::map_dfc(as.character)
+    } else {
+      # Turn each element into a tibble and rowbind them
+      this %<>%
+        mutate(
+          candidate_id = candidate_id,
+          year = year
+        ) %>%
+        explode_categories() %>%
+        select(
+          bill_id,
+          candidate_id,
+          office_id,
+          category_id,
+          vote,
+          everything()
+        )
+    }
+
+    out %<>%
+      bind_rows(this)
+  }
+  out %>%
+    distinct()
+}
