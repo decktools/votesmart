b05d522d2e13f1e7eb780271cda9b974c82a2f02
Amanda Dobbyn <amanda.e.dobbyn@gmail.com>
2020-09-14 10:55:05 -0400

    only look for secrets locally

--- tests/testthat.R
+++ tests/testthat.R
@@ -8,8 +8,9 @@ obama_id <- 9490
 pete_id <- 127151
 warren_id <- 141272
 
-if ("bonanza" %in% installed.packages()) {
-  Sys.setenv(VOTESMART_API_KEY = bonanza::secrets.get("VOTESMART_API_KEY"))
+test_check("votesmart")
+
+if (nchar(Sys.getenv("VOTESMART_API_KEY")) > 0) {
   test_check("votesmart")
 } else {
   message("API key not found.")
